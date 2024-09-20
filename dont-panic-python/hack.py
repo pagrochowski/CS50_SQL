dfba5d44f0fd11430b8a5d51ff491b82d4ff4c7d
pagrochowski <pagrochowski@gmail.com>
2024-09-20 21:20:35 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   dont-panic-python/dont-panic.db
    	modified:   dont-panic-python/hack.py

--- dont-panic-python/hack.py
+++ dont-panic-python/hack.py
@@ -1,1 +1,11 @@
-print("Hacked!")
+from cs50 import SQL
+
+
+db = SQL("sqlite:///dont-panic.db")
+db.execute(
+    """
+    UPDATE "users"
+    SET "password" = 'hacked!'
+    WHERE "username" = 'admin';       
+    """
+)
